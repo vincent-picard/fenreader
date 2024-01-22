82367ff1950858087171838a279ba57084ef17fd
Vincent Picard <vincent.picard1@ac-lyon.fr>
2024-01-22 19:47:14 +0100

    Algebraic notation of pieces

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,43 @@
+use std::fmt;
+
 pub enum Color {
     White,
     Black,
 }
+
+pub enum Piece {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+}
+
+impl fmt::Display for Piece {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Piece::Pawn => write!(f, "P"),
+            Piece::Knight => write!(f, "N"),
+            Piece::Bishop => write!(f, "B"),
+            Piece::Rook => write!(f, "R"),
+            Piece::Queen => write!(f, "Q"),
+            Piece::King => write!(f, "K"),
+        }
+    }
+}
+
+impl Piece {
+    pub fn from_algebraic(s: &str) -> Result<Piece, &'static str> {
+        match s.chars().next() {
+            Some('P') => Ok(Piece::Pawn),
+            Some('N') => Ok(Piece::Knight),
+            Some('B') => Ok(Piece::Bishop),
+            Some('R') => Ok(Piece::Rook),
+            Some('Q') => Ok(Piece::Queen),
+            Some('K') => Ok(Piece::King),
+            _ => Err("Not a valid piece in algebraic notation"),
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use fenreader::board::Color;
+use fenreader::board::{Color, Piece};
 
 fn main() {
     fenreader::test();
     println!("Hello, world!");
 
     let _ctest = Color::White;
+    let ptest = Piece::Rook;
+    println!("My piece is a {} !", ptest);
 }
