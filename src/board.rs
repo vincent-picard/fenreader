6c5b8468874e4c296518b4d0c0ab4fe5dbfec554
Vincent Picard <vincent.picard1@ac-lyon.fr>
2024-01-27 20:40:47 +0100

    putting piece unit test in right file

--- src/board.rs
+++ src/board.rs
@@ -50,32 +50,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn piece_to_string() {
-        assert_eq!("♙", Piece::Pawn.to_string());
-        assert_eq!("♘", Piece::Knight.to_string());
-        assert_eq!("♗", Piece::Bishop.to_string());
-        assert_eq!("♖", Piece::Rook.to_string());
-        assert_eq!("♕", Piece::Queen.to_string());
-        assert_eq!("♔", Piece::King.to_string());
-    }
-
-    #[test]
-    fn colored_piece_to_string() {
-        assert_eq!("♙", ColoredPiece(Color::White, Piece::Pawn).to_string());
-        assert_eq!("♘", ColoredPiece(Color::White, Piece::Knight).to_string());
-        assert_eq!("♗", ColoredPiece(Color::White, Piece::Bishop).to_string());
-        assert_eq!("♖", ColoredPiece(Color::White, Piece::Rook).to_string());
-        assert_eq!("♕", ColoredPiece(Color::White, Piece::Queen).to_string());
-        assert_eq!("♔", ColoredPiece(Color::White, Piece::King).to_string());
-        assert_eq!("♟", ColoredPiece(Color::Black, Piece::Pawn).to_string());
-        assert_eq!("♞", ColoredPiece(Color::Black, Piece::Knight).to_string());
-        assert_eq!("♝", ColoredPiece(Color::Black, Piece::Bishop).to_string());
-        assert_eq!("♜", ColoredPiece(Color::Black, Piece::Rook).to_string());
-        assert_eq!("♛", ColoredPiece(Color::Black, Piece::Queen).to_string());
-        assert_eq!("♚", ColoredPiece(Color::Black, Piece::King).to_string());
-    }
-
-    #[test]
     fn colors_of_squares() {
         assert!(Square::from_coord(0, 0).expect("Should be valid coordinates").color().is_black());
         assert!(Square::from_coord(1, 1).expect("Should be valid coordinates").color().is_black());
